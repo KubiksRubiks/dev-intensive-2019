7ca681cf3dc105c97645dc534b4541721ca6b880
Kubiks Rubiks <KubiksGit@yandex.com>
2020-09-22 21:48:17 +0300

    *transliteration

--- app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -16,4 +16,54 @@ object Utils {
             lastName.trim().first().toString().toUpperCase()
         else -> (firstName!!.trim().first().toString().toUpperCase() + lastName!!.trim().first().toString().toUpperCase())
     }
+
+    fun transliteration(payload: String, divider: String = " "): String {
+        var str: String = ""
+        for (i in 0..payload.length - 1) {
+            when (true) {
+                payload[i].isUpperCase() -> str = str +
+                        replaceRusToEngLetter(payload[i].toString().toLowerCase(), divider).toUpperCase()
+                else -> str = str + replaceRusToEngLetter(payload[i].toString(), divider)
+            }
+        }
+        return str
+    }
+
+    fun replaceRusToEngLetter(letter: String, div: String): String = when (letter) {
+        "а" -> "a"
+        "б" -> "b"
+        "в" -> "v"
+        "г" -> "g"
+        "д" -> "d"
+        "е" -> "e"
+        "ё" -> "e"
+        "ж" -> "zh"
+        "з" -> "z"
+        "и" -> "i"
+        "й" -> "i"
+        "к" -> "k"
+        "л" -> "l"
+        "м" -> "m"
+        "н" -> "n"
+        "о" -> "o"
+        "п" -> "p"
+        "р" -> "r"
+        "с" -> "s"
+        "т" -> "t"
+        "у" -> "u"
+        "ф" -> "f"
+        "х" -> "h"
+        "ц" -> "c"
+        "ч" -> "ch"
+        "ш" -> "sh"
+        "щ" -> "sh'"
+        "ъ" -> ""
+        "ы" -> "i"
+        "ь" -> ""
+        "э" -> "e"
+        "ю" -> "yu"
+        "я" -> "ya"
+        " " -> div
+        else -> letter
+    }
 }
