bfe7862346b6053d67994f8eae1c80e33ca1c32c
KubiksRubiks <41064346+KubiksRubiks@users.noreply.github.com>
2020-09-23 10:44:01 +0300

    *String.stripHtml

--- app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
@@ -7,3 +7,10 @@ fun String.truncate(limit: Int = 16) : String {
         else -> return "${str.substring(0,limit).trim()}..."
     }
 }
+
+fun String.stripHtml(): String {
+    return this
+        .replace(Regex("<[^>]*>"), "") //удаляет HTML-теги
+        .replace(Regex("\\s+"), " ") //удаляет пустые символы (пробелы) между словами если их больше 1
+        .replace(Regex("[&'\"]"), "") //удаляет html escape последовательности ("& < > ' "")
+}
