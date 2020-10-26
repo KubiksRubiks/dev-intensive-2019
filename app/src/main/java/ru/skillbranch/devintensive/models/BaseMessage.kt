17bb564c6893e92d14b24bf120f7b5ced8b93271
Kubiks Rubiks <KubiksGit@yandex.com>
2020-10-26 13:43:56 +0300

    Реализация методов Chat

--- app/src/main/java/ru/skillbranch/devintensive/models/BaseMessage.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/BaseMessage.kt
@@ -1,24 +1,27 @@
 package ru.skillbranch.devintensive.models
 
+import ru.skillbranch.devintensive.models.data.Chat
+import ru.skillbranch.devintensive.models.data.User
 import java.util.*
 
 abstract class BaseMessage(
-    val id: String,
-    val from: User?,
-    val chat: Chat,
-    val isIncoming: Boolean = false,
-    val date: Date = Date()
+        val id: String,
+        val from: User?,
+        val chat: Chat,
+        val isIncoming: Boolean = false,
+        val date: Date = Date(),
+        var isReaded: Boolean = false
 ) {
     abstract fun formatMessage(): String
 
     companion object AbstractFactory {
         private var lastId: Int = -1
-        fun makeMessage(from: User?, chat: Chat, date: Date = Date(), type: String = "text", payload: Any?,  isIncoming: Boolean = false
+        fun makeMessage(from: User?, chat: Chat, date: Date = Date(), type: String = "text", payload: Any?, isIncoming: Boolean = false
         ): BaseMessage {
             lastId++
-            return when(type){
-                "image"->ImageMessage("$lastId",from,chat,isIncoming,date = date,image = payload.toString())
-                "text" -> TextMessage("$lastId",from,chat,isIncoming,date = date,text = payload.toString())
+            return when (type) {
+                "image" -> ImageMessage("$lastId", from, chat, isIncoming, date = date, image = payload.toString())
+                "text" -> TextMessage("$lastId", from, chat, isIncoming, date = date, text = payload.toString())
                 else -> throw IllegalStateException()
             }
         }
