150087e28b2039a9c97f888a0e58f7f46a661c55
Kubiks Rubiks <KubiksGit@yandex.com>
2020-10-29 18:35:43 +0300

    Поиск по RecyclerView MainActivity

--- app/src/main/java/ru/skillbranch/devintensive/viewmodels/MainViewModel.kt
+++ app/src/main/java/ru/skillbranch/devintensive/viewmodels/MainViewModel.kt
@@ -1,22 +1,37 @@
 package ru.skillbranch.devintensive.viewmodels
 
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
+import ru.skillbranch.devintensive.extensions.mutableLiveData
 import ru.skillbranch.devintensive.models.data.ChatItem
 import ru.skillbranch.devintensive.repositories.ChatRepository
 
 
 class MainViewModel : ViewModel() {
+    private val query = mutableLiveData("")
     private val chatRepository = ChatRepository
     private val chats = Transformations.map(ChatRepository.loadChats()) { chats ->
         return@map chats.filter { !it.isArchived }
-                .map { it.toChatItem() }
-                .sortedBy { it.id.toInt() }
+            .map { it.toChatItem() }
+            .sortedBy { it.id.toInt() }
     }
 
     fun getChatData(): LiveData<List<ChatItem>> {
-        return chats
+        val result = MediatorLiveData<List<ChatItem>>()
+
+        val filterF = {
+            val queryStr = query.value!!
+            val chats = chats.value!!
+            result.value = if (queryStr.isEmpty()) chats
+            else chats.filter { it.title.contains(queryStr, true) }
+        }
+
+        result.addSource(chats) { filterF.invoke() }
+        result.addSource(query) { filterF.invoke() }
+
+        return result
     }
 
     fun addToArchive(chatId: String) {
@@ -29,5 +44,9 @@ class MainViewModel : ViewModel() {
         val chat = chatRepository.find(chatId)
         chat ?: return
         chatRepository.update(chat.copy(isArchived = false))
+    }
+
+    fun handleSearchQuery(text: String?) {
+        query.value = text
     }
 }
